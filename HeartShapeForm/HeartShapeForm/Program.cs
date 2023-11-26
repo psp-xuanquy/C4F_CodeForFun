b114ef1a253f5d1863ec2313e1d93a5963d15b46
Basil <144904034+DaoXuanQuy@users.noreply.github.com>
2023-11-27 00:02:45 +0700

    HeartShape code

---
+++ HeartShapeForm/HeartShapeForm/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+class HeartShape
+{
+    static void Main(string[] args)
+    {
+        for (double y = 1.5; y > -1.5; y -= 0.1)
+        {
+            for (double x = -1.5; x < 1.5; x += 0.05)
+            {
+                double a = x * x + y * y - 1;
+                Console.Write(a * a * a - x * x * y * y * y <= 0 ? "Q" : ".");
+            }
+            Console.WriteLine();
+        }
+    }
+}
