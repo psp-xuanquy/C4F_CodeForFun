ecd428cce6ca3d08c1888222879870147dbfeda0
Basil <144904034+DaoXuanQuy@users.noreply.github.com>
2023-12-01 07:51:09 +0700

    new project: CalculateLunarYear

---
+++ CalculateLunarYear/CalculateLunarYear/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+
+Console.Write("Moi ban nhap 1 nam bat ky: ");
+int year = int.Parse(s: Console.ReadLine());
+string[] Can = { "Canh", "Tan", "Nham", "Quy", "Giap", "At", "Binh", "Dinh", "Mau", "Ky" };
+string[] Chi = { "Than", "Dau", "Tuat", "Hoi", "Ti", "Suu", "Dan", "Mao", "Thin", "Ty", "Ngo", "Mui" };
+
+Console.WriteLine($"Nam {year} co nam am lich la: {Can[year % 10].ToUpper()} {Chi[year % 12].ToUpper()}");
