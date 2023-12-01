4abb0fbebf2cdaa00c0562662f4394ca54774814
Basil <144904034+DaoXuanQuy@users.noreply.github.com>
2023-12-01 08:11:08 +0700

    update HeartShape project

--- HeartShapeForm/HeartShapeForm/obj/Debug/net6.0/HeartShapeForm.AssemblyInfo.cs
+++ HeartShapeForm/HeartShapeForm/obj/Debug/net6.0/HeartShapeForm.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HeartShapeForm")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+872bc934a65b12336c943e743b4f1616ae8e4ae3")]
 [assembly: System.Reflection.AssemblyProductAttribute("HeartShapeForm")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HeartShapeForm")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
